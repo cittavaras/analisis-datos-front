1fcdcd11bf1d37d1aec857151729e3c0b744ac3c
unknown <ma.corrolumnos.duoc.cl>
2020-09-03 18:27:50 -0400

    3er avance

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -3,6 +3,8 @@ import {HttpClient, HttpParams} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {map} from "rxjs/operators";
 
+import { AppComponent } from './app.component';
+
 
 @Injectable({
   providedIn: 'root'
@@ -11,10 +13,11 @@ export class ApiService {
 
 constructor( private http:HttpClient ) { }
 
-    llamarModeloLineal(parametro1:number): Observable<any> {
+    llamarModeloLineal(parametro1:Number, parametro2:number): Observable<any> {
 	    return this.http.get('http://localhost:5000/api/llamada', {
             params: new HttpParams()
-                .set('parametro', String(parametro1))
+                .set('parametro1', String(parametro1))
+                .set('parametro2', String(parametro2))
         }).pipe(
             map(res =>  res["payload"])
         );
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { MatInput } from "@angular/material/input";
-import {FormBuilder, Validators, FormGroup} from "@angular/forms";
+import {FormBuilder, Validators, FormGroup, FormControl} from "@angular/forms";
 import {Observable, BehaviorSubject, of} from "rxjs";
 import {catchError, finalize} from "rxjs/operators";
-
+import { debounceTime } from 'rxjs/operators';
 import {ApiService} from "./api.service";
 
 @Component({
@@ -12,26 +12,57 @@ import {ApiService} from "./api.service";
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit{
-	title: string;
-	form: FormGroup;
+    title: string;
+    form: FormGroup;
 
-	constructor(
-	private fb: FormBuilder,
-	private apiService: ApiService) {
-		this.form = fb.group({
-            		parametro1: [null,]
-        	});
-	}
+    constructor(
+    private fb: FormBuilder,
+    private apiService: ApiService) {
 
-	ngOnInit() {
-	    this.title = "Formulario!!!"
+        this.form = fb.group({
+                    parametro1: ['', [Validators.required]],
+                    parametro2: ['', [Validators.required]]
+            });
+    }
+
+    ngOnInit() {
+        this.title = "Data analisis"
         }
-        
-	save() {
-		console.log("Alguien hzo click", this.form.value);
-	        this.apiService.llamarModeloLineal(1000).pipe(
+
+    // save() {
+    //     console.log("Alguien hizo click", this.form.value);
+    //         this.apiService.llamarModeloLineal(1000).pipe(
+    //             catchError(() => of([]))
+    //         )
+    //         .subscribe(title => this.title);
+
+    //     }
+
+
+
+    private buildForm2() {
+    this.form = new FormGroup({
+      parametro1    : new FormControl('', [Validators.required]),
+      parametro2 : new FormControl('', [Validators.required])
+    });
+
+
+
+
+  }
+    save2(event: Event) {
+
+		console.log("Alguien hizo click", this.form.value);
+		const fecinicio=this.form.value.parametro1;
+		const fectermino=this.form.value.parametro2;
+		console.log(fecinicio);
+		console.log(fectermino);
+            this.apiService.llamarModeloLineal(fecinicio,fectermino).pipe(
                 catchError(() => of([]))
             )
-            .subscribe(title => this.title);
+            .subscribe(value => this.form.value);
+        event.preventDefault();
+        const value = this.form.value;
+        console.log(value);
         }
 }
